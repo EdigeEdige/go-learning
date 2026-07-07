040cfb1a676ae0124c5c4e4acbe153180ca97bee
EdigeEdige <syrym.05@list.ru>
2026-07-07 20:09:41 +0500

    Day 1

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 	fmt.Printf("Имя: %s\nЦель обучения: %s\nДата старта: %s\nВозраст: %d\n", "Edige", "Стать программистом", "07.07.2026", 21)
+	// fmt.Println("==============")
+	// fmt.Println("|go-learning |")
+	// fmt.Println("==============")
+
+	// fmt.Printf("%d * %d = %d\n", 7, 8, 7*8)
+	// fmt.Printf("%d / %d = %d\n", 10, 3, 10/3)
+	// fmt.Printf("%d mod %d = %d\n", 10, 3, 10%3)
+
+	fmt.Printf("Книги: %s\nКоличество: %d\nЦена: %.2f\n", "Война и Мир", 10, 500.75)
+}
